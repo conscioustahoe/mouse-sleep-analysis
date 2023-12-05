6edf849e9352e59b0054f75766b26c1354d07bdb
conscioustahoe <ashutoshnarang80@gmail.com>
2023-12-05 19:43:10 +0000

    train model with normalized delta by theta signal

---
+++ README.md
@@ -0,0 +1,6 @@
+# Step 1: Train the model
+Run `train_model.ipynb`
+# Step 2: Latent state inference
+Run `infer_states.ipynb`
+# Step 3: Latent state analysis
+Run `latent_states_analysis.ipynb`
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 pip install numpy matplotlib scipy scikit-learn
 git clone https://github.com/cudamat/cudamat.git && cd cudamat && python setup.py install --user
-# pip install nose && cd test && nosetests && python ../examples/bench_cudamat.py
+pip install nose && cd test && nosetests && python ../examples/bench_cudamat.py
